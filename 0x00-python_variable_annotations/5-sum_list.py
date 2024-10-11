0d28b5b2241ed194e08b4ccbac59ac535b8eff7c
manyala1 <paulmuthama60@gmail.com>
2024-10-11 20:25:26 +0000

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin env python3
+"""
+Complex tyoes - list of floats
+"""
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    return a sum of all nums inside a list
+    """
+    return sum(input_list)
